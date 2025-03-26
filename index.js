ecd3029c74b7b325308fa1a6cd599c28fe35984e
JoseC14 <josecarlosv8474@gmail.com>
2025-03-26 13:29:29 -0300

    feat(route):cria rotas principais

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
-
-const express = require('express')
+import {engine} from 'express-handlebars'
+import express from 'express'
 const app = express()
 const port = 3000
 
+app.engine('handlebars', engine())
+
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 app.use(express.static('public'))
 
+
+
 app.get('/',(req,res) =>{
-    res.send('Hello, World!')
+    res.render('ApresentacaoPlus',{
+        title:"Gerador de apostilas para docentes"
+    })
 })
 
+app.get('/moodleqparser',(req,res) => {
+    res.render('MoodleQParser', {
+        title:"Moodle QParser"
+    })
+})
 
+app.get('/tic2des',(req,res) =>{
+    res.render('Tic2Des', {
+        title:"Grupo de Pesquisa Tic2Des"
+    })
+})
 
 app.listen(port, () =>
 {
