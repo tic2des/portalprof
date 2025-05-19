d3c4804ee242af75260c4e85898c5d2b32440002
JoseC14 <josecarlosv8474@gmail.com>
2025-05-19 14:47:59 -0300

    refactor(style): reestruturado estilo do botoes

--- backend/routes/middleware/auth.js
+++ backend/routes/middleware/auth.js
@@ -4,26 +4,28 @@ dotenv.config()
 const {verify, sign} = jwt
 
 export const isLogged = (req, res, next)=>{
-    if(req.isAuthenticated() || req.user) return next()
-    res.status(401).json({message: "Acesso não autorizado"})
+    if(req.isAuthenticated()){
+
+        req.photo = req.user.photo
+        req.displayName = req.user.displayName
+        return next()
+
+    }
+    res.status(401).send("<h1>Acesso não autorizado</h1>")
 }
 
 export const verifyToken = (req, res, next)=>
 {
     const authHeader = req.headers['authorization'];
-    console.log(req.headers)
-    console.log(`Cabecalho ${authHeader}`)
     var token
     if (authHeader && authHeader.startsWith('Bearer ')) {
       token = authHeader.split(' ')[1]; 
-      console.log(`Token capturado ${token}`)
     }
     const token_decoded = verify(token, process.env.SECRET,(err, decoded)=>{
         if(err){
             console.log(err)
             res.json({Erro:'Token inválido'})
         }
-        console.log(decoded)
         req.user = decoded
         return next()
     })
